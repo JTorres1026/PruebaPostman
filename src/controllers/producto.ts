dce91c571b2560d872e800a7e587ff0bb0a2edbd
Juan Torres <juanestuardoje@gmail.com>
2023-10-05 16:23:16 -0600

    backend

---
+++ src/controllers/producto.ts
@@ -0,0 +1,106 @@
+
+import {Request,Response} from 'express';
+import Producto from '../models/producto';
+
+export const getProducts = async (req:Request, res:Response)=>{
+    const listProducts = await Producto.findAll();
+
+    res.json(listProducts);
+}
+
+    export const getProduct = async (req:Request, res:Response)=>{
+
+        const { id } = req.params;
+
+        const product = await Producto.findByPk(id);
+
+        if(product){
+            res.json(product);
+        }else{
+            res.status(404).json({
+                msg: 'No existe un producto con el id '+id
+            })
+        }
+
+    }
+
+
+    export const deleteProduct = async (req:Request, res:Response)=>{
+
+        const { id } = req.params;
+
+        const product = await Producto.findByPk(id);
+
+        if(!product){
+            res.status(404).json({
+                msg: 'No existe un producto con el id '+id
+            })
+        }else{
+           await product.destroy();
+           res.json({ 
+            msg: 'El producto fue eliminado con éxito'
+           })
+        }
+
+      
+    }
+
+
+    export const postProduct =async (req:Request, res:Response)=>{
+
+        const { body } = req;
+
+    try{
+
+      await Producto.create(body);
+
+      res.json({
+        msg: 'El producto fue agregado con exito'
+      });
+    }catch (error){
+        console.log(error);
+        res.json({
+            msg: 'Ocurrio un problema, comuniquese con el equipo de soporte tecnico'
+          });
+
+    }
+    }
+
+
+    export const updateProduct = async (req:Request, res:Response)=>{
+
+        
+        const { body } = req;
+        const { id } = req.params;
+
+
+        try{
+
+            const product = await Producto.findByPk(id);
+
+        if(product){
+            
+          await product.update(body);
+            res.json({
+                msg: 'El producto fue actualizado con exito'
+            })
+       
+
+        }else{
+            res.status(404).json({
+                msg: 'No existe un producto con el id'+id
+            })
+        
+        }
+
+        }catch(error){
+            console.log(error);
+        res.json({
+            msg: 'Ocurrio un problema, comuniquese con el equipo de soporte tecnico'
+          });
+
+        }
+
+        
+        
+    }
